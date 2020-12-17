66205e676adbdb69b58a4a0ce05ae9c8d16d8e44
Garryd <garrydsmit@gmail.com>
2020-12-17 15:53:24 +0200

    Completed the fade in on scroll for features and testimonials. Added Zoom-in-out affect on fade

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,12 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 
+var featureItem = document.querySelectorAll('.feature-item')
+var testimonialItem = document.querySelectorAll('.testimonial-item')
+
 let mobileMenu = new MobileMenu();
-let revealOnScroll = new RevealOnScroll();
+new RevealOnScroll(featureItem, 75);
+new RevealOnScroll(testimonialItem, 60);
 
 
 //Accept hot module replacements so browser is auto updated with changes tp CSS and JS
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,28 +1,55 @@
+import throttle from 'lodash/throttle';
+import debounce from 'lodash/debounce';
+
 class RevealOnScroll {
 
-  constructor() {
-    this.itemsToReveal = document.querySelectorAll('.feature-item');
+  constructor(item, thresholdPercent) {
+    this.itemsToReveal = item;
+    this.thresholdPercent = thresholdPercent;
+    this.browserHeight = window.innerHeight;
     this.hideInitially();
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
     this.events();
   }
 
   events() {
-    window.addEventListener("scroll", () => {
-      this.itemsToReveal.forEach(el => {
+    window.addEventListener("scroll", this.scrollThrottle);
+    window.addEventListener('resize', debounce(()=>{
+      this.browserHeight = window.innerHeight;
+    },330))
+  }
+
+  calcCaller() {
+    console.log("Scroll function ran <br> window Height: "+this.browserHeight)
+    this.itemsToReveal.forEach(el => {
+      if (el.isRevealed == false) {
         this.checkIfScrolledTo(el);
-      })
+      }
     })
   }
 
   hideInitially() {
-    this.itemsToReveal.forEach(el =>el.classList.add('reveal-item'));
+    this.itemsToReveal.forEach(el =>{
+      el.classList.add('reveal-item')
+      el.isRevealed = false
+    });
+    this.itemsToReveal[this.itemsToReveal.length-1].isLastItem = true;
   }
 
   checkIfScrolledTo(el) {
-    let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) * 100;
-    console.log(el.getBoundingClientRect().y+" / "+window.innerHeight+"*100 = "+scrollPercent)
-    if(scrollPercent > 75 && scrollPercent < 100) {
-      el.classList.add('reveal-item--is-visible');
+    if(window.scrollY + this.browserHeight > el.offsetTop) {
+
+      console.log('Element was calculated');
+      let scrollPercent = (el.getBoundingClientRect().y / this.browserHeight) * 100;
+      
+      if(scrollPercent < this.thresholdPercent) {
+        el.classList.add('reveal-item--is-visible');
+        el.isRevealed= true;
+        if(el.isLastItem) {
+          window.removeEventListener('scroll',this.scrollThrottle)
+        }
+      }
+
     }
   }
 
