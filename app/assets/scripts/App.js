9d250473a38eb13e8639b49d805340aa9105d258
Garryd <garrydsmit@gmail.com>
2020-12-18 22:56:51 +0200

    Added modal on click of "Get in touch" buttons, opening and closing handled. Animation on opening and closing completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,10 +2,12 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var featureItem = document.querySelectorAll('.feature-item')
 var testimonialItem = document.querySelectorAll('.testimonial-item')
 
+new Modal();
 let mobileMenu = new MobileMenu();
 new RevealOnScroll(featureItem, 75);
 new RevealOnScroll(testimonialItem, 60);
